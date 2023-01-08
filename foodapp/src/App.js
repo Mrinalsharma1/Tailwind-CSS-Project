d668f4487e260c7c653e0e9e28491f175c735250
Mrinalsharma1 <checkkro1@gmail.com>
2023-01-08 23:57:21 +0530

    Food-App with Tailwind css and Reactjs

---
+++ foodapp/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Category from './component/Category';
+import Food from './component/Food';
+import HeadlineCards from './component/HeadlineCards';
+import Hero from './component/Hero';
+import Navbar from './component/Navbar';
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <Food />
+      <Category />
+    </div>
+  );
+}
+
+export default App;
